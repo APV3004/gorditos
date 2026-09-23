ad9f125da39a3186f934b338775f44538e2268c0
APV3004 <114157239+APV3004@users.noreply.github.com>
2026-09-23 10:01:38 +0200

    Add cartoApiKey to GORDITOS_CONFIG

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.GORDITOS_CONFIG = {
   supabaseUrl: "https://sxyxithmrrqvzrymkhhp.supabase.co",
-  supabaseAnonKey: "sb_publishable_bsdNhgcFImWdl86hKcGUEg_ZCKAOX7p"
+  supabaseAnonKey: "sb_publishable_bsdNhgcFImWdl86hKcGUEg_ZCKAOX7p",
+  cartoApiKey: "cb1_3usb_1_ea5a3f49b90c8f1a984b6f20"
 };
  
